58dd8fbf6a2b4499970a83ce73feb750f1992b65
ZhangRGK <stamen0630@gmail.com>
2014-02-17 16:40:41 +0800

    修改api与显示内容

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,68 +1,54 @@
 $(function () {
     $("#start_date").on("change",function () {
         var date = $(this).val();
-        if(!validateDate(date)) {
+        if (!validateDate(date)) {
             $(".alert-danger").html("日期输入有问题……").removeClass("hidden");
-            $("#first").empty();
-            $("#second").empty();
-            $("#third").empty();
+            $("#content").empty();
             return false;
         }
-        $.post("/getPlan", {"date": date})
-            .done(loadData).fail(function () {
+        $.post("/loadPlan", {"date": date})
+            .done(loadPlan).fail(function () {
                 $(".alert-danger").html("出错啦！快去看看日志！").removeClass("hidden");
             });
+        return false;
     }).on("keypress", function (event) {
             var date = $(this).val();
-            if(event.keyCode == 13) {
-                if(!validateDate(date)) {
+            if (event.keyCode == 13) {
+                if (!validateDate(date)) {
                     $(".alert-danger").html("日期输入有问题……").removeClass("hidden");
-                    $("#first").empty();
-                    $("#second").empty();
-                    $("#third").empty();
+                    $("#content").empty();
                     return false;
                 }
                 $.post("/getPlan", {"date": date})
-                    .done(loadData).fail(function () {
+                    .done(loadPlan).fail(function () {
                         $(".alert-danger").html("出错啦！快去看看日志！").removeClass("hidden");
                     });
-                return false;
             }
+            return false;
         });
 
-    var validateDate = function(date) {
+    var validateDate = function (date) {
         var reg = new RegExp("^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$");
         return reg.test(date);
     };
-    var loadData = function (data) {
+    var loadPlan = function (data) {
+        var date = new Date($("#start_date").val());
+        var yearAndMonth = (1900+date.getYear()) + "-" + (date.getMonth()+1) + "-";
+        $(".firstDay").html(yearAndMonth+date.getDate());
+        $(".secondDay").html(yearAndMonth+(date.getDate()+1));
+        $(".thirdDay").html(yearAndMonth+(date.getDate()+2));
+        $(".fourthDay").html(yearAndMonth+(date.getDate()+3));
+        $(".fifthDay").html(yearAndMonth+(date.getDate()+4));
+        $(".sixthDay").html(yearAndMonth+(date.getDate()+5));
+        $(".seventhDay").html(yearAndMonth+(date.getDate()+6));
+
         var results = eval(data);
-        var names = results.names;
-        var first = results.firstDay;
-        var second = results.secondDay;
-        var third = results.thirdDay;
-        var firstBody = "";
-        var secondBody = "";
-        var thirdBody = "";
-        //组装 table
-        for (var i in names) {
-            firstBody += "<tr class='tr_" + i + "'><td>" + names[i] + "</td><td>" + first[i] + "</td></tr>";
-            secondBody += "<tr class='tr_" + i + "'><td>" + second[i] + "</td></tr>";
-            thirdBody += "<tr class='tr_" + i + "'><td>" + third[i] + "</td></tr>";
+        var content = "";
+        for (var i in results) {
+            var record = results[i];
+            content += "<tr><td>" + record.name + "</td><td>" + record.firstDay + "</td><td class='visible-sm visible-md visible-lg'>" + record.secondDay + "</td><td class='visible-sm visible-md visible-lg'>" + record.thirdDay + "</td><td class='visible-md visible-lg'>" + record.fourthDay + "</td><td class='visible-md visible-lg'>" + record.fifthDay + "</td><td class='visible-lg'>" + record.sixthDay + "</td><td class='visible-lg'>" + record.seventhDay + "</td></tr>";
         }
-        $("#first").empty().append(firstBody);
-        $("#second").empty().append(secondBody);
-        $("#third").empty().append(thirdBody);
-
-        $("tbody tr").on("mouseover", function () {
-            var className = $(this).attr("class");
-            $("." + className).addClass("active");
-        });
-
-        $("tbody tr").on("mouseout", function () {
-            var className = $(this).attr("class").replace(" active", "");
-            $("." + className).removeClass("active");
-        });
-
+        $("#content").empty().append(content);
         $(".alert-danger").addClass("hidden");
     };
 });
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,33 @@ exports.index = function(req, res){
   res.render('index');
 };
 
-exports.getPlan = function(req,res) {
-    res.json({
-        "names":["唐敲敲","唐敲敲+1","唐敲敲+2","唐敲敲+3","唐敲敲+4"]
-        ,"firstDay":["玩MH一整天","写个爬虫抓毛片","找妹子","使劲作死","傻笑"]
-        ,"secondDay":["玩MH又一天","下毛片","把妹子","被发现","继续傻笑"]
-        ,"thirdDay":["玩MH再一天","看毛片","表白","受死","继续傻笑"]
-    });
-}
+exports.loadPlan = function(req,res) {
+    res.json([{
+        "name":"唐超",
+        "firstDay":"喵喵喵",
+        "secondDay":"汪汪汪",
+        "thirdDay":"吱吱吱",
+        "fourthDay":"咕咕咕",
+        "fifthDay":"嘤嘤嘤",
+        "sixthDay":"嗡嗡嗡",
+        "seventhDay":"咚咚咚"
+    },{
+        "name":"唐超+1",
+        "firstDay":"喵喵喵+1",
+        "secondDay":"汪汪汪+1",
+        "thirdDay":"吱吱吱+1",
+        "fourthDay":"咕咕咕+1",
+        "fifthDay":"嘤嘤嘤+1",
+        "sixthDay":"嗡嗡嗡+1",
+        "seventhDay":"咚咚咚+1"
+    },{
+        "name":"唐超+2",
+        "firstDay":"喵喵喵+2",
+        "secondDay":"汪汪汪+2",
+        "thirdDay":"吱吱吱+2",
+        "fourthDay":"咕咕咕+2",
+        "fifthDay":"嘤嘤嘤+2",
+        "sixthDay":"嗡嗡嗡+2",
+        "seventhDay":"咚咚咚+2"
+    }]);
+};
